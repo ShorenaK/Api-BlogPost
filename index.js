839749db5d083e3edc8badab57473838f9ec8a60
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-14 19:46:20 -0400

    cleaned files from comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const form = document.getElementById("new-post")
 const bodyInput = document.getElementById("post-title")
 const titleInput = document.getElementById("post-body")
 const postDiv = document.getElementById('blog-list')
-const newPostArrays = []
+let newPostArrays = []
 // https - is the part of the protocol 
 
 
@@ -10,25 +10,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 .then(request => request.json())
 .then((data)=> data)
 
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: 'GET'})
-.then((request)=> request.json())
-.then((data)=> {
-    const postArr = data.slice(0,5)
-    let html = ""
-    for (let post of postArr){
-        html += `<h1>${post.title}</h1>
-        <p>${post.body}</p>
-        <hr />
-       
-        `
-
-    }
-   postDiv.innerHTML = html
- 
-})
-
-function render(){
+function renderPost(){
     let html = ""
     for (let newPost of newPostArrays){
        html += `<h1> ${newPost.title}</h1>
@@ -36,8 +18,16 @@ function render(){
                 <hr />
        `
     }
-    postDiv.innerHTML += html
+    postDiv.innerHTML = html
 }
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: 'GET'})
+.then((request)=> request.json())
+.then((data)=> {
+    newPostArrays = data.slice(0,5)
+    renderPost()
+})
+
 form.addEventListener("submit", function(e){
     e.preventDefault()
    const postTitle = titleInput.value
@@ -57,7 +47,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", options )
 .then(res => res.json()) // parsing the data from json to js
 .then(post =>{
    newPostArrays.unshift(post)
-    render()
+    renderPost()
     titleInput.value = ""
     bodyInput.value = ""
 })
@@ -74,62 +64,3 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", options )
 
 
 
-
-
-
-
-
-
-
-
-// let postsArray = []
-// const titleInput = document.getElementById("post-title")
-// const bodyInput = document.getElementById("post-body")
-// const form = document.getElementById("new-post")
-
-// function renderPosts() {
-//     let html = ""
-//     for (let post of postsArray) {
-//         html += `
-//             <h3>${post.title}</h3>
-//             <p>${post.body}</p>
-//             <hr />
-//         `
-//     }
-//     document.getElementById("blog-list").innerHTML = html
-// }
-
-// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-//     .then(res => res.json())
-//     .then(data => {
-//         postsArray = data.slice(0, 5)
-//         renderPosts()
-//     })
-
-// form.addEventListener("submit", function(e) {
-//     e.preventDefault()
-//     const postTitle = titleInput.value
-//     const postBody = bodyInput.value
-//     const data = {
-//         title: postTitle,
-//         body: postBody
-//     }
-    
-//     const options = {
-//         method: "POST",
-//         body: JSON.stringify(data),
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     }
-    
-//     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-//         .then(res => res.json())
-//         .then(post => {
-//             postsArray.unshift(post)
-//             renderPosts()
-//             titleInput.value = ""
-//             bodyInput.value = ""
-//             // form.reset()
-//         })
-// })
